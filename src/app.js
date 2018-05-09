6606ba0059997b92d6161787b65cca5bf704fc02
Kaykai <lag50225@gmail.com>
2018-05-09 21:32:04 +0800

    update 資料表欄位

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ app
         secret: process.env.JWT_SECRET,
         // passthrough: true
     }).unless({
-        path: ['/', /^\/activity/, /^\/qrcode/, /^\/dashboard/, '/api/carousel', '/api/user/login', '/api', '/api/mission', '/api/mission/type', /^\/api\/mission\/id\/.*/, /^\/api\/upload\/.*/]
+        path: ['/', '/api/notification/username', /^\/activity/, /^\/qrcode/, /^\/dashboard/, '/api/carousel', '/api/user/login', '/api', '/api/mission', '/api/mission/type', /^\/api\/mission\/id\/.*/, /^\/api\/upload\/.*/]
     }))
     .use(routes())
 
