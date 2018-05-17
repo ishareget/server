e6f92d0e74553622511bdfa24494f7a9d37701e7
Kaykai <lag50225@gmail.com>
2018-05-17 20:51:03 +0800

    add util class

---
+++ src/util.js
@@ -0,0 +1,5 @@
+const util = {
+    path: ['/', /^\/activity/, /^\/qrcode/, /^\/dashboard/, '/api/carousel', '/api/user/login', '/api', '/api/mission', '/api/mission/type', /^\/api\/mission\/id\/.*/, /^\/api\/upload\/.*/]
+};
+
+module.exports = util;
