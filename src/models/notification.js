de62114848b3f568de485424fc966f6730f2ebdd
jimmy55660 <jumhau860624@gmail.com>
2018-05-18 15:56:03 +0800

    Update noti status

--- src/models/notification.js
+++ src/models/notification.js
@@ -19,9 +19,10 @@ class Notification {
 
     async create(ctx) {
         try {
-            const data = ctx.request.body; 
+            const data = ctx.request.body;
             const connection = await mysql.createConnection(config);
             const sql = `INSERT INTO web_notification (username, groupid, mission_id, noti_time, description, status, type) VALUES ('${data.username}', ${data.groupid}, ${data.mission_id}, '${data.noti_time}', '${data.description}', 0, '${data.type}')`;
+            console.log(sql);
             const [result] = await connection.query(sql);
             connection.close();
             return result;
@@ -33,7 +34,10 @@ class Notification {
     async update(ctx) {
         try {
             const connection = await mysql.createConnection(config);
-            const [result] = await connection.query(`UPDATE web_notification SET status = 1 Where id = ${ctx.request.body.id}`);
+            console.log(ctx);
+            console.log(ctx.request.body);
+            console.log(ctx.request.body.username);
+            const [result] = await connection.query(`UPDATE web_notification SET status = 1 Where username = '${ctx.request.body.username}'`);
             return result;
         } catch (e) {
             return false;
