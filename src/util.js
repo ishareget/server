5ad7dd56e7f6c8ded35b2685e2226b6f28662b1f
Kaykai <lag50225@gmail.com>
2018-05-22 20:04:04 +0800

    update mission group unless

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 const util = {
-    path: ['/', /^\/activity/, /^\/qrcode/, /^\/dashboard/, '/api/carousel', '/api/user/login', '/api', '/api/mission', '/api/mission/type', /^\/api\/mission\/id\/.*/, /^\/api\/upload\/.*/]
+    path: ['/', /^\/activity/, /^\/qrcode/, /^\/dashboard/, '/api/carousel', '/api/user/login', '/api', '/api/mission', '/api/mission/type', /^\/api\/mission\/id\/.*/, /^\/api\/upload\/.*/, /^\/api\/mission\/group.*/]
 };
 
 module.exports = util;
